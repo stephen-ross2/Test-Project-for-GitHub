56e53f021841f1a1a0c7ccd4593ec64b0da10513
Steve <Steve@DESKTOP-CNEJFDN>
2024-11-14 09:19:23 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Test_Project_for_GitHub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
